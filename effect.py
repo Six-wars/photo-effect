96a1f20bb514bfc1fbe599ea91f3a1e93589c729
Samuel Muiruri <muiruri.samuel@gmail.com>
2018-06-22 14:57:37 +0300

    this

--- effect.py
+++ effect.py
@@ -2,6 +2,7 @@ from PIL import Image, ImageFilter
 
 import string
 import random
+import os
 
 def random_filename(path=None, length=None):
 	text = string.ascii_letters + string.digits
@@ -15,7 +16,7 @@ def random_filename(path=None, length=None):
 
 	return path + ''.join( [random.choice(text) for x in length] ) + '.png'
 
-def processor(img_filename):
+def processor(img_filename, delete_sections=False, delete_converts=False):
 	original = Image.open(img_filename)
 
 	img_height, img_width = original.height, original.width
@@ -42,4 +43,26 @@ def processor(img_filename):
 	original_section_filename = random_filename(path="sections/")
 	original_section.save(original_section_filename)
 
-	return None
+	sections = [edge_section, grey_section, original_section]
+
+	merged = Image.new('RGB', (img_width, img_height))
+
+	offset = 0
+	for section in sections:
+		merged.paste(section, (offset, 0))
+		offset += section.size[0]
+
+	merged_filename = random_filename(path="merged/")
+	merged.save(merged_filename)
+
+	if delete_sections:
+		os.remove(grey_section_filename)
+		os.remove(edge_section_filename)
+		os.remove(original_section_filename)
+
+	if delete_converts:
+		os.remove(grey_filename)
+		os.remove(edged_filename)
+
+
+	return merged_filename
